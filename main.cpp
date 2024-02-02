51ef3e645230b4d8005c79b6c6caa433de21ddfd
rocco2300 <grigoreandrey23@gmail.com>
2024-02-02 18:53:41 +0200

    graphics hello world

--- main.cpp
+++ main.cpp
@@ -1,6 +1,46 @@
+#include <GL/gl3w.h>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
+const int Width = 1280;
+const int Height = 720;
+
 int main() {
-    std::cout << "Hello world!\n";
+    if (!glfwInit()) {
+        std::cerr << "GLFW init failed!\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    GLFWwindow* window = glfwCreateWindow(Width, Height, "Raytracer", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (gl3wInit()) {
+        std::cerr << "GL3W init failed!\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        glViewport(0, 0, width, height);
+        glClearColor(160.f / 255.f, 202.f / 255.f, 146.f / 255.f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
